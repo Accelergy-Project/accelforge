bb12d9f549a7ec32bc5158c6cf1f2b5a4e06b3a9
tanner-andrulis <andrulis@mit.edu>
2025-08-08 20:20:11 +0000

    Remove unused metrics

--- fastfusion/mapper/metrics.py
+++ fastfusion/mapper/metrics.py
@@ -7,10 +7,8 @@ from operator import or_
 class Metrics(Flag):
     LATENCY = auto()
     ENERGY = auto()
-    PER_COMPONENT_ENERGY = auto()
     RESERVATIONS = auto()
     # OCCUPANCY = auto()
-    OFF_CHIP_ACCESSES = auto()
     OP_INTENSITY = auto()
     DEBUG = auto()
     PER_COMPONENT_ACCESSES_ENERGY = auto()
@@ -19,5 +17,5 @@ class Metrics(Flag):
 
     @classmethod
     def all_metrics(cls):
-        return reduce(or_, iter(cls), cls.LATENCY) ^ Metrics.OP_INTENSITY
+        return reduce(or_, iter(cls), cls.LATENCY)
 
