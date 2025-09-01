f2600fe4b43dc5f9aefe4ea04b1489bd8e7b742d
Renggeng Zheng <rzhengcontacts@gmail.com>
2025-09-01 04:31:49 -0400

    annotated the buffet class

--- fastfusion/model/looptree/reuse/__init__.py
+++ fastfusion/model/looptree/reuse/__init__.py
@@ -1,8 +1,21 @@
 from dataclasses import dataclass
 from fastfusion.frontend.mapping import TensorName
+from fastfusion.frontend.workload.workload import EinsumName
 
 @dataclass(eq=True, frozen=True)
 class Buffet:
+    """
+    A logical buffer that stores a tensor, an einsum operating on it, and the
+    level the buffer exists on in hardware.
+
+    :param tensor:  The tensor held by the buffet.
+    :param einsum:  An einsum operating on the tensor.
+    :param level:   The abstract hardware level the buffet resides in.
+
+    :type tensor:   TensorName
+    :type einsum:   EinsumName
+    :type level:    str
+    """
     tensor: TensorName
-    einsum: str
+    einsum: EinsumName
     level: str
