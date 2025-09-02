1e837bc3e523a1dd17ad97247b5542b7b3876feb
Renggeng Zheng <rzhengcontacts@gmail.com>
2025-09-02 10:22:26 -0400

    blocked by TODO line 167

--- fastfusion/model/looptree/reuse/isl/mapping_to_isl/skews_from_mapping.py
+++ fastfusion/model/looptree/reuse/isl/mapping_to_isl/skews_from_mapping.py
@@ -1,17 +1,177 @@
+"""
+Relevant Name Changes:
+-   BufferID -> ComponentName
+"""
+from collections import defaultdict
+from typing import Callable, List, Tuple
 import islpy as isl
 
-from dataclasses import dataclass
+from fastfusion.frontend.mapping import (
+    Mapping,
+    MappingNode,
+    # Iterations
+    Iteration,
+    Spatial,
+    Temporal,
+    # Splits
+    Pipeline,
+    Sequential,
+    # Logical hardware features
+    Storage,
+    Compute,
+)
+from fastfusion.frontend.workload.workload import TensorName, Workload
 
-from fastfusion.frontend.mapping import Mapping
+from fastfusion.model.looptree.mapping_utilities import get_paths
+from fastfusion.model.looptree.reuse import ComponentName
+from fastfusion.model.looptree.reuse.isl.isl_functions import (
+    dim_projector_mask,
+    insert_equal_dims_map
+)
 
-from .types import *
+from .types import (
+    # Bookkeeping objects
+    BufferTensorEinsum,
+    Buffet,
+    Skew,
+    # Tags
+    Tag,
+    TemporalTag,
+    SpatialTag,
+    PipelineTag,
+    SequentialTag,
+)
 
-
-def skews_from_mapping(mapping, workload) -> dict[BufferTensorEinsum, Skew]:
+def skews_from_mapping(mapping: Mapping, workload: Workload) -> dict[BufferTensorEinsum, Skew]:
+    """
+    Given a mapping ...
+    TODO: Fill this in
+    """
     result: dict[BufferTensorEinsum, Skew] = {}
 
     for path in get_paths(mapping):
-        SkewFromPath(path, workload, result)
+        leaf: Compute = path[-1]
+
+        # Get the last storage node in path for a particular buffet.
+        buffer_to_last_storage_node: dict[ComponentName, MappingNode] = {}
+        buffer_node: List[Tuple[ComponentName, MappingNode]] = []
+        all_buffer_tensors: List[Tuple[ComponentName, TensorName]] = []
+
+        node: MappingNode
+        for node in path:
+            match node:
+                case Storage():
+                    buffer: ComponentName = node.component
+                    buffer_to_last_storage_node[buffer] = node
+                    buffer_node.append((buffer, node))
+                    all_buffer_tensors.append((buffer, node.tensor))
+                case Compute():
+                    compute: ComponentName = node.compute
+                    buffer_to_last_storage_node[compute] = node
+                    buffer_node.append((compute, node))
+
+        node_to_current_buffer: dict[MappingNode, ComponentName] = {}
+        buffer_idx: int = 0
+        for node in path:
+            cur_buf, cur_buf_last_node = buffer_node[buffer_idx]
+            node_to_current_buffer[node] = cur_buf
+
+            if node == cur_buf_last_node:
+                buffer_idx += 1
+
+        # Generate tags, map, and which dims (and tags) should be removed per buffer.
+        # TODO: Figure out what SpaceTime is called now.
+        tags: List[Tag] = []
+        removal_map: isl.Map = isl.Map.from_multi_aff(
+            isl.MultiAff.identity_on_domain_space(
+                isl.Space.alloc(isl.DEFAULT_CONTEXT, 0, 0, 0).domain()
+            )
+        )
+        buffer_storage_past: set[tuple[ComponentName, TensorName]] = set()
+        buffer_fully_complete: set[ComponentName] = set()
+        buffer_to_dim_removal_mask: defaultdict[
+            Tuple[ComponentName, TensorName], List[bool]
+        ] = defaultdict(list)
+
+        def add_tag(
+            tag: Tag,
+            mask_condition: Callable[[ComponentName], bool] = (
+                lambda buffer: buffer in buffer_fully_complete
+            )
+        ) -> None:
+            """
+            Performs necessary modifications to removal_map and removal_mask to
+            accomodate tagging.
+
+            :param tag:             The tag to add.
+            :param mask_condition:  Boolean resolution for the removal mask.
+
+            Postconditions
+            --------------
+            -   `tags` has another tag appended to it.
+            -   `removal_map` has an input and output dimension added that are equal
+                to each other.
+            -   `removal_mask` has a new entry.
+            """
+            nonlocal tags
+            tags.append(tag)
+            nonlocal removal_map
+            removal_map = insert_equal_dims_map(
+                removal_map,
+                removal_map.dim(isl.dim_type.in_),
+                removal_map.dim(isl.dim_type.out),
+                1
+            )
+
+            nonlocal all_buffer_tensors
+            nonlocal buffer_to_dim_removal_mask
+            for buffer_tensor in all_buffer_tensors:
+                removal_mask = buffer_to_dim_removal_mask[buffer_tensor]
+                buffer = buffer_tensor[0]
+                removal_mask.append(mask_condition(buffer))
+
+        for node in path:
+            match node:
+                case Storage():
+                    buffer_storage_past.add((node.component, node.tensor))
+                    if node == buffer_to_last_storage_node[node.component]:
+                        buffer_fully_complete.add(node.component)
+                case Iteration():
+                    tag: Tag
+                    if isinstance(node, Temporal):
+                        tag: Tag = TemporalTag()
+                    elif isinstance(node, Spatial):
+                        tag: Tag = SpatialTag(0, node_to_current_buffer[node])
+                    else:
+                        raise ValueError(f"Type {type(node)} is an iteration not in space or time.")
+
+                    # TODO: Verify logical equivalence to:
+                    # https://github.com/NVlabs/timeloop/blob/32370826fdf1aa3c8deb0c93e6b2a2fc7cf053aa/src/loop-analysis/mapping-to-isl/fused-mapping-to-isl.cpp#L660-L671
+                    add_tag(tag, lambda buffer: (
+                        (buffer in buffer_fully_complete) or
+                        (isinstance(node, Temporal) and (buffer in buffer_storage_past))
+                    ))
+                case Pipeline():
+                    add_tag(PipelineTag())
+                case Sequential():
+                    add_tag(SequentialTag())
+
+        for buffer_tensor in all_buffer_tensors:
+            mask: List[bool] = buffer_to_dim_removal_mask[buffer_tensor]
+            domain: isl.Set = removal_map.domain()
+            projector: isl.Map = dim_projector_mask(domain.get_space(), mask)
+            removal_projection: isl.Map = projector.apply_range(removal_map)
+
+            buffer_tags: List[Tag] = [tags[i] for i in range(len(tags)) if mask[i]]
+
+            # TODO: This buffet structure makes no sense in this context:
+            # https://github.com/NVlabs/timeloop/blob/32370826fdf1aa3c8deb0c93e6b2a2fc7cf053aa/src/loop-analysis/mapping-to-isl/fused-mapping-to-isl.cpp#L740-L743
+            buffet_to_skew[Buffet(
+                tensor=buffer_tensor[1],
+                einsum=leaf.einsum,
+                level=leaf.compute
+            )]
+
 
 
 def skew_from_path(
