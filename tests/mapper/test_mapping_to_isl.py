efdfabbffd7c1d77438301d35a7f019b7dc91c90
Renggeng Zheng <rzhengcontacts@gmail.com>
2025-09-12 14:51:19 -0400

    blacked test

--- tests/mapper/test_mapping_to_isl.py
+++ tests/mapper/test_mapping_to_isl.py
@@ -10,16 +10,21 @@ from fastfusion.frontend.workload._isl import get_rank_variable_bounds
 from fastfusion.frontend.mapping import Mapping
 
 from fastfusion.model.looptree.reuse.isl.mapping_to_isl import analyze_mapping
-from fastfusion.model.looptree.reuse.isl.mapping_to_isl.types import MappingAnalysisResult
+from fastfusion.model.looptree.reuse.isl.mapping_to_isl.types import (
+    MappingAnalysisResult,
+)
 
 TEST_CONFIG_PATH: Path = Path(__file__).parent / "configs"
+
 
 class TestMappingToIsl(unittest.TestCase):
 
     def test_conv1d(self):
         # Loads in the CONV1D Config
         CONV1D_CONFIG_PATH: Path = TEST_CONFIG_PATH
-        workload: Workload = Workload.from_yaml(CONV1D_CONFIG_PATH / "conv1d.workload.yaml")
+        workload: Workload = Workload.from_yaml(
+            CONV1D_CONFIG_PATH / "conv1d.workload.yaml"
+        )
 
         mapping: Mapping = Mapping.from_yaml(CONV1D_CONFIG_PATH / "conv1d.mapping.yaml")
         occupancies: MappingAnalysisResult = analyze_mapping.occupancies_from_mapping(
@@ -31,7 +36,7 @@ class TestMappingToIsl(unittest.TestCase):
                 soln: isl.Map = isl.Map.read_from_str(
                     isl.DEFAULT_CONTEXT,
                     "{ [P1, P0, R] -> [P=8*P1 + P0] : "
-                    "0 <= R < 3 and 0 <= P1 < 2 and 0 <= P0 < 8}"
+                    "0 <= R < 3 and 0 <= P1 < 2 and 0 <= P0 < 8}",
                 )
                 print(occupancy.map_)
                 assert occupancy.map_ == soln
@@ -39,9 +44,13 @@ class TestMappingToIsl(unittest.TestCase):
     def test_two_conv1d(self):
         # Loads in the CONV1D Config
         CONV1D_CONFIG_PATH: Path = TEST_CONFIG_PATH
-        workload: Workload = Workload.from_yaml(CONV1D_CONFIG_PATH / "two_conv1d.workload.yaml")
+        workload: Workload = Workload.from_yaml(
+            CONV1D_CONFIG_PATH / "two_conv1d.workload.yaml"
+        )
 
-        mapping: Mapping = Mapping.from_yaml(CONV1D_CONFIG_PATH / "two_conv1d.mapping.yaml")
+        mapping: Mapping = Mapping.from_yaml(
+            CONV1D_CONFIG_PATH / "two_conv1d.mapping.yaml"
+        )
         occupancies: MappingAnalysisResult = analyze_mapping.occupancies_from_mapping(
             mapping, workload
         )
@@ -53,19 +62,7 @@ class TestMappingToIsl(unittest.TestCase):
                 soln: isl.Map = isl.Map.read_from_str(
                     isl.DEFAULT_CONTEXT,
                     "{ [P1, P0, R] -> [P=8*P1 + P0] : "
-                    "0 <= R < 3 and 0 <= P1 < 2 and 0 <= P0 < 8}"
+                    "0 <= R < 3 and 0 <= P1 < 2 and 0 <= P0 < 8}",
                 )
-                # assert occupancy.map_ == soln       
-                print(occupancy.map_) 
-
-
-
-
-
-
-
-
-
-                
-
-        
+                # assert occupancy.map_ == soln
+                print(occupancy.map_)
