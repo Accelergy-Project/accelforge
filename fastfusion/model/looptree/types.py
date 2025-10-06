84ec2b5c5869e1504864f0648cdc898ac604811d
Renggeng Zheng <rzhengcontacts@gmail.com>
2025-10-06 15:14:45 -0400

    actually added the move location

---
+++ fastfusion/model/looptree/types.py
@@ -0,0 +1,31 @@
+"""
+Contains shared classes of analysis.
+"""
+
+from dataclasses import dataclass
+from typing import TypeAlias
+
+from fastfusion.frontend.mapping import TensorName
+from fastfusion.frontend.workload.workload import EinsumName
+
+ComponentName: TypeAlias = str
+
+
+@dataclass(eq=True, frozen=True)
+class Buffet:
+    """
+    A logical buffer that stores a tensor, an einsum operating on it, and the
+    level the buffer exists on in hardware.
+
+    :param tensor:  The tensor held by the buffet.
+    :param einsum:  An einsum operating on the tensor.
+    :param level:   The abstract hardware level the buffet resides in.
+
+    :type tensor:   TensorName
+    :type einsum:   EinsumName
+    :type level:    str
+    """
+
+    tensor: TensorName
+    einsum: EinsumName
+    level: ComponentName
