a7bdc6551488dee708f51f0cc69856d40269698f
tanner-andrulis <andrulis@mit.edu>
2025-12-26 10:13:48 -0500

    Specification -> spec

--- fastfusion/__init__.py
+++ fastfusion/__init__.py
@@ -3,10 +3,10 @@ from fastfusion.frontend import config
 from fastfusion.frontend import constraints
 from fastfusion.frontend import mapping
 from fastfusion.frontend import renames
-from fastfusion.frontend import specification
+from fastfusion.frontend import spec
 from fastfusion.frontend import variables
 from fastfusion.frontend import workload
-from fastfusion.frontend.specification import Specification, Spec
+from fastfusion.frontend.spec import Spec, Spec
 from fastfusion.mapper.FFM import Metrics
 from fastfusion.util import set_n_parallel_jobs
 import fastfusion.mapper as mapper
